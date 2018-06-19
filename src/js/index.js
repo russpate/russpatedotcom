c05bf7fd3acaa2916f014bfd11b0ba9625e25c0f
Russ Pate <russ@russpate.com>
2018-06-19 18:59:25 -0400

    updated the options on tilt

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,7 @@
-// For the fancy button 
-// from Tobias Reich
-// View it on CodePen here: https://codepen.io/electerious/pen/rroqdL
+'use strict'
 
-'use strict';
-
-var docStyle = document.documentElement.style;
-var aElem = document.querySelector('.fancy-button');
-var boundingClientRect = aElem.getBoundingClientRect();
-
-aElem.onmousemove = function (e) {
-
-  var x = e.clientX - boundingClientRect.left;
-  var y = e.clientY - boundingClientRect.top;
-
-  var xc = boundingClientRect.width / 2;
-  var yc = boundingClientRect.height / 2;
-
-  var dx = x - xc;
-  var dy = y - yc;
-
-  docStyle.setProperty('--rx', dy / -1 + 'deg');
-  docStyle.setProperty('--ry', dx / 10 + 'deg');
-};
-
-aElem.onmouseleave = function (e) {
-
-  docStyle.setProperty('--ty', '0');
-  docStyle.setProperty('--rx', '0');
-  docStyle.setProperty('--ry', '0');
-};
-
-aElem.onmousedown = function (e) {
-
-  docStyle.setProperty('--tz', '-25px');
-};
-
-document.body.onmouseup = function (e) {
-
-  docStyle.setProperty('--tz', '-12px');
-};
+$('.tilt-button').tilt({
+  scale: 1.3,
+  speed: 500,
+  transition: true,
+})
